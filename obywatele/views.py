c1022dcae03721baa1487d1b9160a47b172c8b48
Adrian Miedzinski <miedziu@gmail.com>
2026-04-27 21:56:25 +0200

    fix: bug: chat activity does not list the real messages of the person selected #69

--- obywatele/views.py
+++ obywatele/views.py
@@ -1082,12 +1082,13 @@ def citizen_czaty(request: HttpRequest, pk: int):
     qs = Room.objects.filter(allowed=target_user, public=True).order_by('-last_activity')
     rows = []
     for room in qs:
-        last_msg = Message.objects.filter(room=room).order_by('-time').first()
-        rows.append({
-            'room': room,
-            'room_name': room.displayed_name(request.user),
-            'last_msg': last_msg,
-        })
+        last_msg = Message.objects.filter(room=room, sender=target_user).order_by('-time').first()
+        if last_msg:
+            rows.append({
+                'room': room,
+                'room_name': room.displayed_name(request.user),
+                'last_msg': last_msg,
+            })
     template = 'obywatele/_citizen_czaty_partial.html' if request.headers.get('X-Requested-With') == 'XMLHttpRequest' else 'obywatele/citizen_czaty.html'
     return render(request, template, {
         'target_user': target_user,
